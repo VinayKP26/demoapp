e426fa4ca314c43d8f0c602694cc572c0cdbc2f0
VinayKP26 <45734406+VinayKP26@users.noreply.github.com>
2023-11-18 18:43:25 +0530

    Added the code of all three routes (#2)
    * added the basic code and structures
    
    * added the code snipt for all three route
    
    ---------
    
    Co-authored-by: vinaykp26 <vinaykp410@gmaiil.com>

--- controllers/healthCheckController.go
+++ controllers/healthCheckController.go
@@ -3,5 +3,5 @@ package controllers
 import "net/http"
 
 func (c *Controllers) HealthCheckController(w http.ResponseWriter, r *http.Request) {
-
+	w.WriteHeader(http.StatusOK)
 }
--- controllers/infoController.go
+++ controllers/infoController.go
@@ -3,5 +3,7 @@ package controllers
 import "net/http"
 
 func (c *Controllers) InfoController(w http.ResponseWriter, r *http.Request) {
-
+	body := "This is demoapp. This api is providing information to you"
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(body))
 }
